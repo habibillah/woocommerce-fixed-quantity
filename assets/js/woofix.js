eca07da78f8f70017246b067cbaf8e3f726d6870
Habibillah <habibillah@gmail.com>
2016-06-15 07:26:14 +0700

    add timeout to wait button active

--- assets/js/woofix.js
+++ assets/js/woofix.js
@@ -1,5 +1,8 @@
 jQuery(document).ready(function($) {
-    $('.woofix_qty_on_cart').on('change', function() {
-        $('input[name="update_cart"]').click();
+    $('body').on('change', '.woofix_qty_on_cart', function() {
+        var that = this;
+        setTimeout(function () {
+            $(that).closest('form').find('input[name="update_cart"]').click();
+        }, 300);
     });
 });
