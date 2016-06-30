5d63698d913bd8d2c7cf1d5bc8ee0f1bb8e15311
Habibillah <habibillah@gmail.com>
2016-06-30 08:02:40 +0700

    fix contruct data in user space

--- classes/woofix-utility.php
+++ classes/woofix-utility.php
@@ -11,16 +11,32 @@ if (!class_exists('WooAdminFixedQuantity')) {
          */
         public static function isFixedQtyPrice($postId)
         {
-            $returnValue = false;
-            $fixedQuantityData = get_post_meta($postId, '_woofix', true);
-            if (!empty($fixedQuantityData)) {
-                $fixedQuantityData = json_decode(html_entity_decode($fixedQuantityData), true);
-                if (count($fixedQuantityData['woofix']) > 0) {
-                    $returnValue = $fixedQuantityData;
+            $current_user = wp_get_current_user();
+            $current_user_roles = $current_user->roles;
+            $default_role = get_option(WOOFIXOPT_DEFAULT_ROLE, WOOFIXCONF_DEFAULT_ROLE);
+
+
+            $returnValue = self::constructQtyData($postId, $current_user_roles);
+
+            if (empty($returnValue)) {
+                if (!in_array($default_role, $current_user_roles)) {
+                    array_unshift($current_user_roles, $default_role);
                 }
+                $returnValue = self::constructQtyData($postId, $current_user_roles);
             }
 
-            return $returnValue;
+            if (empty($returnValue))
+                return false;
+
+            $woofixData = array_unique($returnValue, SORT_REGULAR);
+            usort($woofixData, function ($a, $b) {
+                if ($a['woofix_qty'] == $b['woofix_qty'])
+                    return 0;
+
+                return ($a['woofix_qty'] < $b['woofix_qty']) ? -1 : 1;
+            });
+            $woofixData = apply_filters('woofix_sort_qty_data_to_show', $woofixData);
+            return array('woofix' => $woofixData);
         }
 
         /**
@@ -43,6 +59,43 @@ if (!class_exists('WooAdminFixedQuantity')) {
             }
 
             return null;
+        }
+
+        /**
+         * @param $postId
+         * @param $current_user_roles
+         * @return array
+         */
+        private static function constructQtyData($postId, $current_user_roles)
+        {
+            $returnValue = array();
+
+            $fixedQuantityText = get_post_meta($postId, '_woofix', true);
+            $fixedQuantityData = json_decode(html_entity_decode($fixedQuantityText), true);
+            if (!empty($fixedQuantityData['woofix'])) {
+
+                foreach ($fixedQuantityData['woofix'] as $key => $value) {
+
+                    if (is_numeric($key)) {
+                        $returnValue[] = $value;
+                        continue;
+                    }
+
+                    if (!in_array($key, $current_user_roles))
+                        continue;
+
+                    foreach ($value as $qty_data) {
+                        if (empty($qty_data['woofix_qty']))
+                            continue;
+
+                        if (empty($qty_data['woofix_disc']) && empty($qty_data['woofix_price']))
+                            continue;
+
+                        $returnValue[] = $qty_data;
+                    }
+                }
+            }
+            return $returnValue;
         }
     }
 }
